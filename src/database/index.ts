f7e2dc5a498108e66db68f6f0d2afc6de3595492
Thomas Albuquerque <tealbthomas@gmail.com>
2023-08-09 19:17:19 -0300

    Now with env-var in database file configuration.

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,7 @@
 import { Sequelize } from "sequelize"
-import dotenv from "dotenv";
-dotenv.config();
-// const a = process.env.DATABASE_URL
-const DATABASE_URL = 'postgres://nano_payments_admin:nano_payments_admin_password@localhost:5432/nano_payments_db'
+import * as env from "env-var"
+
+const DATABASE_URL = env.get("DATABASE_URL").required().asString()
 
 export const sequelize = new Sequelize(DATABASE_URL, {
   define: {
